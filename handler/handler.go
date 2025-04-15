b68c7b5576ab925c86ac5f4bf308d13d083388e0
Matteus Moreno <matteus_moreno@live.com>
2025-04-15 11:31:03 -0300

    refatorando a logica de rotas com handler

---
+++ handler/handler.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func CreateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "POST Opening",
+	})
+}
+func ShowOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET Opening",
+	})
+}
+func UpdateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "PUT Opening",
+	})
+}
+func DeleteOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "DELETE Opening",
+	})
+}
+func ListOpeningsHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET Openings",
+	})
+}
