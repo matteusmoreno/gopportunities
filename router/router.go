3c4d40a4addbdf8d4f178c2b7eec6399544a0afd
Matteus Moreno <matteus_moreno@live.com>
2025-04-15 11:11:08 -0300

    adicionando rotas do CRUD

--- router/router.go
+++ router/router.go
@@ -5,11 +5,7 @@ import "github.com/gin-gonic/gin"
 func Initialize() {
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(router)
 
 	err := router.Run(":8090")
 	if err != nil {
