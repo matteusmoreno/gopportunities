0bd9cfa2d39b07ca91ee8ddbd401d4a318b62a73
Matteus Moreno <matteus_moreno@live.com>
2025-04-15 15:10:28 -0300

    adicionando a funcionalidade de detalahar uma opening

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -2,11 +2,29 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/matteusmoreno/gopportunities/schemas"
 	"net/http"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	if id == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "ID is required",
+		})
+		return
+	}
+
+	opening := schemas.Opening{}
+	err := db.First(&opening, id).Error
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "Opening not found",
+		})
+		return
+	}
+
 	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
+		"opening": opening,
 	})
 }
--- router/routes.go
+++ router/routes.go
@@ -11,7 +11,7 @@ func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
 		v1.POST("/opening", handler.CreateOpeningHandler)
-		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.GET("/opening/:id", handler.ShowOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.DELETE("/opening/:id", handler.DeleteOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
