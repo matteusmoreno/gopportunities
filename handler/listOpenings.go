07e13284577d10440a32e748dbec301533e7a500
Matteus Moreno <matteus_moreno@live.com>
2025-04-15 15:05:12 -0300

    adicionando a funcionalidade e listar todas as openings

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -2,11 +2,19 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/matteusmoreno/gopportunities/schemas"
 	"net/http"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
+	var openings []schemas.Opening
+	if err := db.Find(&openings).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error()})
+		return
+	}
+
 	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Openings",
+		"openings": openings,
 	})
 }
